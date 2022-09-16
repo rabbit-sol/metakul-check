1367f6cf688aa3d101906d1eb5849f9859b83592
SUBHAM KUNWAR <68282784+poisendev91@users.noreply.github.com>
2022-09-16 19:46:49 +0530

    ass

--- src/static.js
+++ src/static.js
@@ -11,11 +11,11 @@ const staticData = {
     },
     nft: {
       q: "What is NFT? ",
-      a: "Meta-kul is a metaverse based nft project which aims to spread awareness on blockchain WEB 3.0, NFT's, etc and we want our educational system to be changed. Reforming the vedic education system in Metaverse where every students will be treated equally and can work on there special talent and polish them.",
+        a: " An NFT is a digital asset that represents real-world objects like art, music, in-game items and videos. They are bought and sold online, frequently with cryptocurrency.",
     },
     blockChain: {
-      q: "What is Blockchain? ",
-          a: "Meta-kul is a metaverse based nft project which aims to spread awareness on blockchain WEB 3.0, NFT's, etc and we want our educational system to be changed. Reforming the vedic education system in Metaverse where every students will be treated equally and can work on there special talent and polish them.",
+        q: "What can you do Metakul NFT you own? ",
+          a: "1.You can sell it for a profit or hold it to earn $KULL token to use for breeding or in-game.",
       },
   },
 };
